b4eff372c6e376c980455be307adfe0eea123372
Leonardo Henrique da Rocha Araujo <leonardohra.gh@gmail.com>
2024-11-12 15:29:27 -0300

    Criando o item do repositório

---
+++ src/components/ItemRepo/index.js
@@ -0,0 +1,16 @@
+import ItemContainer from "./styles";
+
+
+function ItemRepo() {
+  return (
+    <ItemContainer>
+        <h3>H3</h3>
+        <p>P</p>
+        <a href="#">Ver repositório</a>
+        <a href="#" className="remover">Remover</a>
+        <hr />
+    </ItemContainer>
+  )
+}
+
+export default ItemRepo;
---
+++ src/components/ItemRepo/styles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+const ItemContainer = styled.div`
+    width: 80%;
+    height: 20px;
+
+    h3{
+        font-size: 32px;
+        color: #FAFAFA;
+    }
+    
+    p{
+        font-size: 16px;
+        color: #FAFAFA60;
+        margin-bottom: 20px;
+    }
+
+    a.remover{
+        color: #FF0000;
+        margin-top: 20px;
+    }
+
+    hr{
+        color: #FAFAFA60;
+        margin: 20px 0;
+    }
+`
+
+export default ItemContainer;
